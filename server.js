fbc5315cc1c1d8a026f603d7b8121940c59b5db2
Lim Jing Ru <jingru0314@gmail.com>
2023-07-03 11:15:19 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ var app = require('./controller/app.js');
 var server = app.listen(8081, function () {
     var port = server.address().port;
     console.log('Web App Hosted at http://localhost:%s/B/selectCountry.html', port);
-});
+})
